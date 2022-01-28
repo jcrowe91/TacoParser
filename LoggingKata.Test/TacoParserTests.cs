b3ab92415765c0eb8f0693b8120c575dc34fe815
jcrowe91 <jcrowe9119@gmail.com>
2022-01-28 16:15:54 -0600

    Finished embedded loops.

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -7,39 +7,41 @@ namespace LoggingKata.Test
     {
         [Fact]
         public void ShouldDoSomething()
-        {
-            // TODO: Complete Something, if anything
-
-            //Arrange
-            var tacoParser = new TacoParser();
-
-            //Act
-            var actual = tacoParser.Parse("34.073638, -84.677017, Taco Bell Acwort...");
-
-            //Assert
+        {         
+                      
+            var tacoParser = new TacoParser();                   
+            var actual = tacoParser.Parse("34.073638, -84.677017, Taco Bell Acwort...");          
             Assert.NotNull(actual);
-
         }
 
         [Theory]
         [InlineData("34.073638, -84.677017, Taco Bell Acwort...", -84.677017)]
         public void ShouldParseLongitude(string line, double expected)
-        {
-            // TODO: Complete - "line" represents input data we will Parse to
-            //       extract the Longitude.  Your .csv file will have many of these lines,
-            //       each representing a TacoBell location
+        {                                  
+            var parser = new TacoParser();      
+            var actual = parser.Parse(line);      
+            Assert.Equal(actual.Location.Longitude, expected);
+        }
 
-            //Arrange
-            var parser = new TacoParser();
-
-            //Act
-            var actual = parser.Parse(line);
-            //Assert
-            Assert.Equal(actual, expected);
+        [Theory]
+        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", 34.073638)]
+        public void ShouldParseLatitude(string line, double expected)
+        {                    
+            var parser = new TacoParser();      
+            var actual = parser.Parse(line);      
+            Assert.Equal(actual.Location.Latitude, expected);
         }
 
 
+        #region INSTRUCTIONS
+        // TODO: Complete Something, if anything
+        // TODO: Complete - "line" represents input data we will Parse to
+        //       extract the Longitude.  Your .csv file will have many of these lines,
+        //       each representing a TacoBell location
+        // TODO: Complete - "line" represents input data we will Parse to
+        //       extract the Longitude.  Your .csv file will have many of these lines,
+        //       each representing a TacoBell location
         //TODO: Create a test ShouldParseLatitude
-
+        #endregion
     }
 }
